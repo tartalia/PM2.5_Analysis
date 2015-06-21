01567b74d17125955fc63bb183197f7cad5e6f49
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-06-21 14:43:12 -0300

    Added plot 2; changed line in plot 1

--- plot2.R
+++ plot2.R
@@ -7,15 +7,15 @@ SCC <- readRDS("Source_Classification_Code.rds")
 nei <- data.table(NEI)
 
 # sum Emissions by SCC (subject) and year
-gdt <- nei[, sum(Emissions), by = "year"]
-
-png("plot1.png")
+gdt <- nei[, sum(Emissions), by = "year,fips"]
+gdt <- gdt[(fips == "24510")]
+png("plot2.png")
 
 # supress cientific notation on y label
 options(scipen=5)
 
 # plot the graph
-plot(gdt$year, gdt$V1, type = "l", lwd = "3", ylab="Total Amount of PM2.5 Emissions (in tons) ", xlab="Year", col="red", main="Total Emission of PM2.5 by Year (1999 - 2008)")
+plot(gdt$year, gdt$V1, type="l", lwd = "3", ylab="Total Amount of PM2.5 Emissions (in tons) ", xlab="Year", col="red", main="Total Emission of PM2.5 in Baltimore City by Year (1999 - 2008)")
 
 # add linear model line
 r <- lm(V1 ~ year, data = gdt)
