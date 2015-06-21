2d50de08dc8468e63e651ab9c83e99d1fc78767f
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-06-21 19:50:40 -0300

    changed plot 5

--- plot5.R
+++ plot5.R
@@ -8,23 +8,29 @@ SCC <- readRDS("Source_Classification_Code.rds")
 nei <- data.table(NEI)
 
 # Cut SCC to get all source that is related to motor vehicle
-# I'm assuming all SCC where EI.Sector are in mobile - aircrafts, locomotives, non-road equipments, marine vessels, heavy and light vehicles
+# I'm assuming that motor vehicle is all SCC with EI.Sector in mobile. eg:
+# - aircrafts, locomotives, non-road equipments, marine vessels, heavy and light vehicles
 
-# Grepping in EI.Sector by Coal, subset SCC by the select rows, and select SCC column
+# Grepping in EI.Sector by Mobile, subset SCC by the select rows, and select SCC column
 scc <- SCC[grep("Mobile - ", SCC$EI.Sector), "SCC"]
 
-# subset NEI dataset to select only SCC related to coal combustion
+# subset NEI dataset to select only SCC related to motor vehicle
 nei <- nei[(SCC %in% scc),]
 
-# sum emission grouped by year
+# sum emission grouped by year and fips
 gdt <- nei[, sum(Emissions), by = "year,fips"]
 
 # select Baltimore City
 gdt <- gdt[(fips == "24510")]
 
-# plot the graph with a linear regression line
-png("plot5.png", width=800, height=600)
-qplot(year, V1, data = gdt, geom = "line", ylab="Total Amount of PM2.5 Emissions (in tons) ", xlab="Year", 
-      main="Total Emission of PM2.5 Related to Motor Vehicle in Baltimore City by Year (1999 - 2008)") + geom_smooth(size = 2, linetype = 3, method = "lm", se = FALSE)
-dev.off()
+# transform variables in factors
+gdt$year <- factor(gdt$year)
 
+# plot the graph
+png("plot5.png", width=800, height=600)
+g <- ggplot(aes(year, V1), data = gdt) + geom_bar(stat = "identity", width = .5) 
+g <- g + labs(title = "Total Emission of PM2.5 Related to Motor Vehicle In Baltimore City by Year (1999 - 2008))")
+g <- g + labs(x = "Year", y = "Total Amount of PM2.5 Emissions (in tons)")
+print(g)
+
+dev.off()
